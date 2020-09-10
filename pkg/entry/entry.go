d6219d2f266885f035545089e0b3faa39d505bd8
bipulpr <bipul.pramanick@flipkart.com>
2020-09-10 23:17:36 +0530

    Remove gorm model

--- pkg/entry/entry.go
+++ pkg/entry/entry.go
@@ -1,14 +1,10 @@
 package entry
 
-import "github.com/jinzhu/gorm"
-
 type ReportedValue struct {
 	Fmt string `json:"fmt"`
 }
 
 type Entry struct {
-	gorm.Model
-
 	AsOfDate      string        `json:"asOfDate"`
 	PeriodType    string        `json:"periodType"`
 	ReportedValue ReportedValue `json:"reportedValue"`
